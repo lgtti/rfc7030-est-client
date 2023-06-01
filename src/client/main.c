fcf34cfa026d01327130595bb879b3b78d574545
yyi9884 <yyi9884@infocert.it>
2023-06-01 15:44:30 +0200

    Improve docs

--- src/client/main.c
+++ src/client/main.c
@@ -71,6 +71,24 @@ static struct cag_option options[] = {
         .access_name = "basic-auth",
         .value_name = "USERNAME:PASSWORD",
         .description = "Authentication for EST Server connection using http basic auth. Value should be username:password (but password can be empty)"
+    },
+    {
+        .identifier = 'o',
+        .access_name = "output",
+        .value_name = "PEM FILENAME PATH",
+        .description = "Filename and path used to save the output of the command"
+    },
+    {
+        .identifier = 'f',
+        .access_name = "output-ca",
+        .value_name = "PEM FILENAME PATH",
+        .description = "Filename and path used to save the CA certificates"
+    },
+    {
+        .identifier = 'z',
+        .access_name = "output-crt",
+        .value_name = "PEM FILENAME PATH",
+        .description = "Filename and path used to save the Enrolled/Renewed certificate"
     }
 };
 
@@ -97,6 +115,16 @@ static size_t read_file(const char *name, const char *flags, char *output) {
     return res_len;
 }
 
+static bool_t write_file(const char *name, const char *flags, char *input) {
+    FILE *fp = fopen(name, flags);
+    if(!fp) {        
+        LOG_ERROR(("Failed to open %s from resource file\n", name))
+        exit(EXIT_FAILURE);
+    }
+
+    return fputs(input, fp) != EOF;
+}
+
 int main(int argc, char *argv[]) {
     bool_t enroll = EST_FALSE;
     bool_t renew = EST_FALSE;
@@ -121,6 +149,9 @@ int main(int argc, char *argv[]) {
     const char *p12_filename = NULL;
     const char *p12_password = NULL;
     const char *basic_auth = NULL;
+    const char *output = NULL;
+    const char *output_ca = NULL;
+    const char *output_crt = NULL;
 
     cag_option_context context;
     char identifier;
@@ -166,6 +197,15 @@ int main(int argc, char *argv[]) {
         case 'b':
             basic_auth = cag_option_get_value(&context);
             break;
+        case 'o':
+            output = cag_option_get_value(&context);
+            break;
+        case 'f':
+            output_ca = cag_option_get_value(&context);
+            break;
+        case 'z':
+            output_crt = cag_option_get_value(&context);
+            break;
         case 'h':
             printf("Usage: rfc7030-est-client [OPTIONs] [enroll|renew|cacerts]\n");
             cag_option_print(options, CAG_ARRAY_SIZE(options), stdout);
@@ -181,7 +221,7 @@ int main(int argc, char *argv[]) {
 
     int operation_param_num = argc - context.index;
     if (operation_param_num != 1) {
-        printf("Missing operation enroll, renew, cacerts\n");
+        LOG_ERROR(("Missing operation enroll, renew, cacerts\n"));
         printf("Usage: rfc7030-est-client [OPTIONs] [enroll|renew|cacerts]\n");
         cag_option_print(options, CAG_ARRAY_SIZE(options), stdout);
         return EXIT_FAILURE;
@@ -207,14 +247,14 @@ int main(int argc, char *argv[]) {
     size_t cacerts_pem_len = 100000;
 
     if(host == NULL) {
-        printf("Missing host parameter\n");
+        LOG_ERROR(("Missing host parameter\n"));
         failed = EST_TRUE;
     }
 
     if(chain_filename) {
         chain_content_len = read_file(chain_filename, "rt", chain_content);
     } else if(!skip_tls_verify) {
-        printf("Please choose one of insecure flag or server chain flag.\n");
+        LOG_ERROR(("Please choose one of insecure flag or server chain flag.\n"));
         failed = EST_TRUE;
     }
 
@@ -233,7 +273,7 @@ int main(int argc, char *argv[]) {
 
     if(enroll || renew) { 
         if(!csr_filename) {
-            printf("Missing csr parameter\n");
+            LOG_ERROR(("Missing csr parameter\n"));
             failed = EST_TRUE;
         } else {
             csr_content_len = read_file(csr_filename, "rt", csr_content);
@@ -241,22 +281,22 @@ int main(int argc, char *argv[]) {
         }
 
         if(!p12_filename && !basic_auth) {
-            printf("At least p12 or basic auth flag required\n");
+            LOG_ERROR(("At least p12 or basic auth flag required\n"));
             failed = EST_TRUE;
         } else {
             if(p12_filename) {
-                printf("Use mTLS X.509 Certificate authentication\n");
+                LOG_INFO(("Use mTLS X.509 Certificate authentication\n"));
 
                 p12_content_len = read_file(p12_filename, "rb", p12_content);
                 if(!estCfg->parse_p12(p12_content, p12_content_len, p12_password, &rfcConfig.auth, &err)) {
-                    printf("Invalid p12 (code=%d,native=%d,subsystem=%d): %s\n", err.code, err.native, err.subsystem, err.human);
+                    LOG_ERROR(("Invalid p12 (code=%d,native=%d,subsystem=%d): %s\n", err.code, err.native, err.subsystem, err.human));
                     failed = EST_TRUE;
                 }
             } else if(basic_auth) {
-                printf("Use HTTP Basic Auth authentication\n");
+                LOG_INFO(("Use HTTP Basic Auth authentication\n"));
 
                 if(!estCfg->parse_basicauth(basic_auth, &rfcConfig.auth, &err)) {
-                    printf("Invalid basicauth (code=%d,native=%d,subsystem=%d): %s\n", err.code, err.native, err.subsystem, err.human);
+                    LOG_ERROR(("Invalid basicauth (code=%d,native=%d,subsystem=%d): %s\n", err.code, err.native, err.subsystem, err.human));
                     failed = EST_TRUE;
                 }
             }
@@ -272,30 +312,51 @@ int main(int argc, char *argv[]) {
 
         if(renew) {
             if(!rfc7030_renew_certificate(&rfcConfig, cacerts_pem, cacerts_pem_len, enrolled, 5000, &err)) {
-                printf("Renew failed (code=%d,native=%d,subsystem=%d): %s\n", err.code, err.native, err.subsystem, err.human);
+                LOG_ERROR(("Renew failed (code=%d,native=%d,subsystem=%d): %s\n", err.code, err.native, err.subsystem, err.human));
                 return EXIT_FAILURE;
             }
         } else {
             if(!rfc7030_request_certificate(&rfcConfig, cacerts_pem, cacerts_pem_len, enrolled, 5000, &err)) {
-                printf("Enrollment failed (code=%d,native=%d,subsystem=%d): %s\n", err.code, err.native, err.subsystem, err.human);
+                LOG_ERROR(("Enrollment failed (code=%d,native=%d,subsystem=%d): %s\n", err.code, err.native, err.subsystem, err.human));
                 return EXIT_FAILURE;
             }
         }
         
-        printf("CACerts:\n");
-        printf("%s\n", cacerts_pem);
-        printf("Enrolled certificate:\n");
-        printf("%s\n", enrolled);
+        LOG_INFO(("CACerts:\n"));
+        LOG_INFO(("%s\n", cacerts_pem));
+        LOG_INFO(("Enrolled certificate:\n"));
+        LOG_INFO(("%s\n", enrolled));
+
+        if(output || output_ca) {
+            if(!write_file(output ? output : output_ca, "wt", cacerts_pem)) {
+                LOG_ERROR(("Failed to write output\n"));
+                return EXIT_FAILURE;
+            }
+        }
+
+        if(output || output_crt) {
+            if(!write_file(output ? output : output_crt, "a+", enrolled)) {
+                LOG_ERROR(("Failed to write output\n"));
+                return EXIT_FAILURE;
+            }
+        }
     }
 
     if(cacerts) {
         if(!rfc7030_request_cachain(&rfcConfig.opts, cacerts_pem, cacerts_pem_len, &err)) {
-            printf("CAcerts failed (code=%d,native=%d,subsystem=%d): %s\n", err.code, err.native, err.subsystem, err.human);
+            LOG_ERROR(("CAcerts failed (code=%d,native=%d,subsystem=%d): %s\n", err.code, err.native, err.subsystem, err.human));
             return EXIT_FAILURE;
         }
         
-        printf("CACerts:\n");
-        printf("%s\n", cacerts_pem);
+        LOG_INFO(("CACerts:\n"));
+        LOG_INFO(("%s\n", cacerts_pem));
+
+        if(output || output_ca) {
+            if(!write_file(output ? output : output_ca, "wt", cacerts_pem)) {
+                LOG_ERROR(("Failed to write output\n"));
+                return EXIT_FAILURE;
+            }
+        }
     }
 
     return EXIT_SUCCESS;
